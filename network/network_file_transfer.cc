simutrans/simutrans

FIX: Fatal error when querying gameinfo of servers running ancient versions
git-svn-id: svn://localhost/simutrans/trunk@9576 8aca7d54-2c30-db11-9de9-000461428c89

network/network_file_transfer.cc
@@ -143,18 +143,20 @@ const char *network_gameinfo(const char *cp, gameinfo_t *gi)
 
 		if (err == NULL) {
 			// now into gameinfo
-			const loadsave_t::file_status_t err_code = fd.rd_open( filename );
+			const loadsave_t::file_status_t status = fd.rd_open( filename );
 
-			if(  err_code == loadsave_t::FILE_STATUS_OK  ) {
-				gameinfo_t *pgi = new gameinfo_t( &fd );
-				*gi = *pgi;
-				delete pgi;
-				fd.close();
+			if(  status != loadsave_t::FILE_STATUS_OK  ) {
+				// some more insets, while things may have failed
+				err = (status == loadsave_t::FILE_STATUS_ERR_FUTURE_VERSION) ? "Server version too new" : "Server busy";
+			}
+			else if (fd.is_version_less(SIM_VERSION_MAJOR, SIM_SERVER_MINOR)) {
+				err = "Server version too old";
 			}
 			else {
-				// some more insets, while things may have failed
-				err = (err_code == loadsave_t::FILE_STATUS_ERR_FUTURE_VERSION) ? "Server version too new" : "Server busy";
+				*gi = gameinfo_t( &fd );
 			}
+
+			fd.close();
 		}
 		dr_remove( filename );
 end:

soukouki/simutrans-extended

git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9576
8aca7d54-2c30-db11-9de9-000461428c89
# Conflicts:
#	simutrans/history.txt

network/network_file_transfer.cc
@@ -143,18 +143,20 @@ const char *network_gameinfo(const char *cp, gameinfo_t *gi)
 
 		if (err == NULL) {
 			// now into gameinfo
-			const loadsave_t::file_status_t err_code = fd.rd_open( filename );
+			const loadsave_t::file_status_t status = fd.rd_open( filename );
 
-			if(  err_code == loadsave_t::FILE_STATUS_OK  ) {
-				gameinfo_t *pgi = new gameinfo_t( &fd );
-				*gi = *pgi;
-				delete pgi;
-				fd.close();
+			if(  status != loadsave_t::FILE_STATUS_OK  ) {
+				// some more insets, while things may have failed
+				err = (status == loadsave_t::FILE_STATUS_ERR_FUTURE_VERSION) ? "Server version too new" : "Server busy";
+			}
+			else if (fd.is_version_ex_less(EX_VERSION_MAJOR, EX_SAVE_MINOR)) {
+				err = "Server version too old";
 			}
 			else {
-				// some more insets, while things may have failed
-				err = (err_code == loadsave_t::FILE_STATUS_ERR_FUTURE_VERSION) ? "Server version too new" : "Server busy";
+				*gi = gameinfo_t( &fd );
 			}
+
+			fd.close();
 		}
 		dr_remove( filename );
 end: