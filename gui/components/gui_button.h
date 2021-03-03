180552895f93533e86543e4920c3ae57ec1f6f64
Markus Pristovsek <prissi@physik.tu-berlin.de>
2021-03-03 14:34:45 +0000

    ADD: new button type imagebox for having a delete (and many more graphical buttons)
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9669 8aca7d54-2c30-db11-9de9-000461428c89
    
    # Conflicts:
    #	gui/components/gui_schedule.cc

--- gui/components/gui_button.h
+++ gui/components/gui_button.h
@@ -40,6 +40,7 @@ public:
 		square = 1,
 		box,
 		roundbox,
+		imagebox,
 		arrowleft,
 		arrowright,
 		arrowup,
@@ -53,12 +54,14 @@ public:
 		square_state     = square     | state,
 		box_state        = box        | state,
 		roundbox_state   = roundbox   | state,
+		imagebox_state   = imagebox   | state,
 		arrowright_state = arrowright | state,
 		arrowup_state    = arrowup    | state,
 		arrowdown_state  = arrowdown  | state,
 		automatic = 256,
 		square_automatic    = square_state | automatic,
 		box_state_automatic = box_state    | automatic,
+		imagebox_automatic  = imagebox     | automatic,
 		posbutton_automatic = posbutton    | automatic,
 
 		flexible = 1 << 9
@@ -92,8 +95,10 @@ private:
 	const char *text;
 
 	const char *translated_text;
-
-	koord3d targetpos;
+	union {
+		koord3d targetpos;
+		image_id img;
+	};
 	// any click will go to this world
 	static karte_ptr_t welt;
 
@@ -134,6 +139,9 @@ public:
 	 */
 	void set_targetpos( const koord k ); // assuming this is on map ground
 	void set_targetpos3d( const koord3d k ) { targetpos = k; }
+
+	// only relevant for imagebox
+	void set_image(image_id id) { img = id; }
 
 	/**
 	 * Set the displayed text of the button when not to translate
