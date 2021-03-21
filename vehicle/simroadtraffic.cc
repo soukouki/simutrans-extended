603da49193ab6899d9b248226926c22caaace24f
Frederick Hayward <freddyhayward@gmail.c>
2021-03-21 20:19:53 +1100

    fix: private vehicles had effective priorty over convoys at junctions

--- vehicle/simroadtraffic.cc
+++ vehicle/simroadtraffic.cc
@@ -729,9 +729,9 @@ bool private_car_t::can_enter_tile(grund_t *gr)
 				next_90direction = this->calc_direction(pos_next, pos_next_next);
 				dt = get_blocking_vehicle(gr, NULL, this_direction, next_direction, next_90direction, this, next_lane);
 				if(  !dt  ) {
-					// This possibly made traffic too cautious at junctions, causing delays. However, precisely what this did
-					// and why it did it remain unclear, so retaining for reference.
-					//dt = get_blocking_vehicle( test, NULL, next_direction, next_90direction, next_90direction, this, next_lane);
+					// This may cause private cars to be too cautious at junctions, but is necessary to avoid giving
+					// them effective priority over convoys.
+					dt = get_blocking_vehicle( test, NULL, next_direction, next_90direction, next_90direction, this, next_lane);
 				}
 			}
 			// this fails with two crossings together; however, I see no easy way out here ...
