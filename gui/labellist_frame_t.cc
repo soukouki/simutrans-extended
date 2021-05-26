b94e2d6d9c11cc2ea3412e700584663fa07e0431
Markus Pristovsek <prissi@physik.tu-berlin.de>
2021-05-26 06:54:50 +0000

    CHG: label list now saved reloaded and up to standard
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9816
    8aca7d54-2c30-db11-9de9-000461428c89
    
    # Conflicts:
    #	gui/curiositylist_frame_t.cc
    #	gui/labellist_frame_t.cc
    #	gui/labellist_frame_t.h

--- gui/labellist_frame_t.cc
+++ gui/labellist_frame_t.cc
@@ -10,6 +10,7 @@
 #include "../obj/label.h"
 #include "../simworld.h"
 
+char labellist_frame_t::name_filter[256];
 
 static const char *sort_text[labellist::SORT_MODES] = {
 	"hl_btn_sort_name",
@@ -28,16 +29,18 @@ labellist_frame_t::labellist_frame_t() :
 	scrolly(gui_scrolled_list_t::windowskin, labellist_stats_t::compare)
 {
 	set_table_layout(1,0);
-	add_table(3, 2);
+	add_table(2,2);
 	{
 		// 1st row
 		new_component<gui_label_t>("hl_txt_sort");
-		new_component<gui_label_t>("Filter:");
 
-		filter.init( button_t::square_automatic, "Active player only");
-		filter.pressed = labellist_stats_t::filter;
-		filter.add_listener( this );
-		add_component(&filter);
+		add_table(2, 1);
+		{
+			new_component<gui_label_t>("Filter:");
+			name_filter_input.set_text(name_filter, lengthof(name_filter));
+			add_component(&name_filter_input);
+		}
+		end_table();
 
 		// 2nd row
 		add_table(2,1);
@@ -60,24 +63,33 @@ labellist_frame_t::labellist_frame_t() :
 		}
 		end_table();
 
-		new_component<gui_empty_t>();
+		add_table(3,1);
+		{
+			new_component<gui_margin_t>(LINESPACE);
 
-		if (!welt->get_settings().regions.empty()) {
-			//region_selector
-			region_selector.new_component<gui_scrolled_list_t::const_text_scrollitem_t>(translator::translate("All regions"), SYSCOL_TEXT);
+			if (!welt->get_settings().regions.empty()) {
+				//region_selector
+				region_selector.new_component<gui_scrolled_list_t::const_text_scrollitem_t>(translator::translate("All regions"), SYSCOL_TEXT);
 
-			for (uint8 r = 0; r < welt->get_settings().regions.get_count(); r++) {
-				region_selector.new_component<gui_scrolled_list_t::const_text_scrollitem_t>(translator::translate(welt->get_settings().regions[r].name.c_str()), SYSCOL_TEXT);
+				for (uint8 r = 0; r < welt->get_settings().regions.get_count(); r++) {
+					region_selector.new_component<gui_scrolled_list_t::const_text_scrollitem_t>(translator::translate(welt->get_settings().regions[r].name.c_str()), SYSCOL_TEXT);
+				}
+				region_selector.set_selection(labellist_stats_t::region_filter);
+				region_selector.set_width_fixed(true);
+				region_selector.set_size(scr_size(D_BUTTON_WIDTH*1.5, D_EDIT_HEIGHT));
+				region_selector.add_listener(this);
+				add_component(&region_selector);
 			}
-			region_selector.set_selection(labellist_stats_t::region_filter);
-			region_selector.set_width_fixed(true);
-			region_selector.set_size(scr_size(D_BUTTON_WIDTH*1.5, D_EDIT_HEIGHT));
-			region_selector.add_listener(this);
-			add_component(&region_selector);
+			else {
+				new_component<gui_empty_t>();
+			}
+
+			filter.init( button_t::square_automatic, "Active player only");
+			filter.pressed = labellist_stats_t::filter;
+			filter.add_listener( this );
+			add_component(&filter);
 		}
-		else {
-			new_component<gui_empty_t>();
-		}
+		end_table();
 	}
 	end_table();
 
@@ -93,6 +105,9 @@ labellist_frame_t::labellist_frame_t() :
 
 void labellist_frame_t::fill_list()
 {
+	strcpy(last_name_filter, name_filter);
+	label_count = welt->get_label_list().get_count();
+
 	scrolly.clear_elements();
 	FOR(slist_tpl<koord>, const& pos, welt->get_label_list()) {
 		if (labellist_stats_t::region_filter && (labellist_stats_t::region_filter - 1) != welt->get_region(pos)) {
@@ -103,7 +118,9 @@ void labellist_frame_t::fill_list()
 		// some old version games don't have label nor name.
 		// Check them to avoid crashes.
 		if(label  &&  name  &&  (!labellist_stats_t::filter  ||  (label  &&  (label->get_owner() == welt->get_active_player())))) {
-			scrolly.new_component<labellist_stats_t>(pos);
+			if(  name_filter[0] == 0  ||  utf8caseutf8(name, name_filter)  ) {
+				scrolly.new_component<labellist_stats_t>(pos);
+			}
 		}
 	}
 	scrolly.sort(0);
@@ -154,9 +171,32 @@ bool labellist_frame_t::action_triggered( gui_action_creator_t *comp,value_t v)
 
 void labellist_frame_t::draw(scr_coord pos, scr_size size)
 {
-	if(  count_label() != (uint32)scrolly.get_count()  ) {
+	if(  label_count != welt->get_label_list().get_count()  ||  strcmp(last_name_filter, name_filter)  ) {
 		fill_list();
 	}
 
 	gui_frame_t::draw(pos, size);
+}
+
+
+void labellist_frame_t::rdwr(loadsave_t* file)
+{
+	scr_size size = get_windowsize();
+
+	size.rdwr(file);
+	scrolly.rdwr(file);
+	file->rdwr_str(name_filter, lengthof(name_filter));
+	file->rdwr_byte(labellist_stats_t::sort_mode);
+	file->rdwr_bool(labellist_stats_t::sortreverse);
+	file->rdwr_bool(labellist_stats_t::filter);
+	file->rdwr_byte(labellist_stats_t::region_filter);
+	if (file->is_loading()) {
+		scrolly.sort(0);
+		sortedby.set_selection(labellist_stats_t::sort_mode);
+		sort_order.pressed = labellist_stats_t::sortreverse;
+		filter.pressed = labellist_stats_t::filter;
+		region_selector.set_selection(labellist_stats_t::region_filter);
+		fill_list();
+		set_windowsize(size);
+	}
 }
