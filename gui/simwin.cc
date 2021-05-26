0da44b036a49e72bb5923383de546f9645aded15
Markus Pristovsek <prissi@physik.tu-berlin.de>
2021-05-26 02:15:53 +0000

    CHG: factory list now saved reloaded and up to standard
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9815
    8aca7d54-2c30-db11-9de9-000461428c89
    
    # Conflicts:
    #	gui/factorylist_frame_t.cc
    #	gui/factorylist_frame_t.h
    #	gui/factorylist_stats_t.cc
    #	gui/factorylist_stats_t.h
    
    NOTE: The text filter merge is pending.

--- gui/simwin.cc
+++ gui/simwin.cc
@@ -68,6 +68,7 @@
 #include "depotlist_frame.h"
 #include "halt_list_frame.h"
 #include "curiositylist_frame_t.h"
+#include "factorylist_frame_t.h"
 #include "display_settings.h"
 #include "optionen.h"
 
@@ -615,6 +616,7 @@ void rdwr_all_win(loadsave_t *file)
 					case magic_halt_list:      w = new halt_list_frame_t(); break;
 					case magic_citylist_frame_t: w = new citylist_frame_t(); break;
 					case magic_curiositylist:  w = new curiositylist_frame_t(); break;
+					case magic_factorylist:    w = new factorylist_frame_t(); break;
 					case magic_color_gui_t:    w = new color_gui_t(); break;
 					case magic_optionen_gui_t: w = new optionen_gui_t(); break;
 
