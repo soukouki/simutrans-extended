e51ff552df2fa68583811dc948692e76ff11d3d1
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2021-06-07 00:21:24 +0900

    CHG: schedule number display size can be specified

--- gui/components/gui_schedule_item.cc
+++ gui/components/gui_schedule_item.cc
@@ -79,16 +79,17 @@ void gui_colored_route_bar_t::draw(scr_coord offset)
 }
 
 
-gui_schedule_entry_number_t::gui_schedule_entry_number_t(uint number_, uint8 p_col, uint8 style_)
+gui_schedule_entry_number_t::gui_schedule_entry_number_t(uint number_, uint8 p_col, uint8 style_, scr_size size_)
 {
 	number = number_ + 1;
 	style = style_;
 	p_color_idx = p_col;
-	set_size(scr_size(D_ENTRY_NO_WIDTH, D_ENTRY_NO_HEIGHT));
+	size=size_;
+	set_size(size);
 
 	lb_number.set_align(gui_label_t::centered);
-	lb_number.set_size(scr_size(size.w, D_LABEL_HEIGHT));
-	lb_number.set_pos(scr_coord(0,2));
+	lb_number.set_size(size);
+	lb_number.set_pos(scr_coord(0,(size.h-D_LABEL_HEIGHT)/2));
 	add_component(&lb_number);
 }
 
@@ -103,30 +104,30 @@ void gui_schedule_entry_number_t::draw(scr_coord offset)
 	// draw the back image
 	switch (style) {
 		case number_style::halt:
-			display_filled_roundbox_clip(pos.x+offset.x,   pos.y+offset.y,   size.w,   D_ENTRY_NO_HEIGHT,   base_colval, false);
+			display_filled_roundbox_clip(pos.x+offset.x,   pos.y+offset.y,   size.w,   size.h,   base_colval, false);
 			text_colval = color_idx_to_rgb(COL_WHITE);
 			break;
 		case number_style::interchange:
-			display_filled_roundbox_clip(pos.x+offset.x,   pos.y+offset.y,   size.w,   D_ENTRY_NO_HEIGHT,   base_colval, false);
-			display_filled_roundbox_clip(pos.x+offset.x+2, pos.y+offset.y+2, size.w-4, D_ENTRY_NO_HEIGHT-4, color_idx_to_rgb(COL_WHITE), false);
+			display_filled_roundbox_clip(pos.x+offset.x,   pos.y+offset.y,   size.w,   size.h,   base_colval, false);
+			display_filled_roundbox_clip(pos.x+offset.x+2, pos.y+offset.y+2, size.w-4, size.h-4, color_idx_to_rgb(COL_WHITE), false);
 			break;
 		case number_style::depot:
 			for (uint8 i = 0; i < 3; i++) {
 				const scr_coord_val w = (size.w/2) * (i+1)/4;
 				display_fillbox_wh_clip_rgb(pos.x+offset.x + (size.w - w*2)/2, pos.y+offset.y + i, w*2, 1, color_idx_to_rgb(91), false);
 			}
-			display_fillbox_wh_clip_rgb(pos.x+offset.x, pos.y+offset.y + 3, size.w, D_ENTRY_NO_HEIGHT - 3, color_idx_to_rgb(91), false);
+			display_fillbox_wh_clip_rgb(pos.x+offset.x, pos.y+offset.y + 3, size.w, size.h - 3, color_idx_to_rgb(91), false);
 			text_colval = color_idx_to_rgb(COL_WHITE);
 			break;
 		case number_style::none:
-			display_fillbox_wh_clip_rgb(pos.x+offset.x + (size.w - D_ENTRY_NO_WIDTH/2)/2, pos.y+offset.y, D_ENTRY_NO_WIDTH/2, D_ENTRY_NO_HEIGHT, base_colval, true);
+			display_fillbox_wh_clip_rgb(pos.x+offset.x + (size.w - D_ENTRY_NO_WIDTH/2)/2, pos.y+offset.y, D_ENTRY_NO_WIDTH/2, size.h, base_colval, true);
 			break;
 		case number_style::waypoint:
-			display_fillbox_wh_clip_rgb(pos.x+offset.x + D_ENTRY_NO_WIDTH/4, pos.y+offset.y, D_ENTRY_NO_WIDTH/2, D_ENTRY_NO_HEIGHT, base_colval, true);
-			display_filled_circle_rgb(pos.x+offset.x + size.w/2, pos.y+offset.y + D_ENTRY_NO_HEIGHT/2, D_ENTRY_NO_HEIGHT/2, base_colval);
+			display_fillbox_wh_clip_rgb(pos.x+offset.x + D_ENTRY_NO_WIDTH/4, pos.y+offset.y, D_ENTRY_NO_WIDTH/2, size.h, base_colval, true);
+			display_filled_circle_rgb(pos.x+offset.x + size.w/2, pos.y+offset.y + size.h/2, size.h/2, base_colval);
 			break;
 		default:
-			display_fillbox_wh_clip_rgb(pos.x+offset.x, pos.y+offset.y, size.w, D_ENTRY_NO_HEIGHT, base_colval, false);
+			display_fillbox_wh_clip_rgb(pos.x+offset.x, pos.y+offset.y, size.w, size.h, base_colval, false);
 			text_colval = color_idx_to_rgb(COL_WHITE);
 			break;
 	}
