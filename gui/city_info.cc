eb4ca11083f9420e6857bdbf6a9722dbe90055f8
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2021-08-26 00:17:36 +0900

    CHG: Move passenger destination mapping to a dedicated tab

--- gui/city_info.cc
+++ gui/city_info.cc
@@ -11,7 +11,6 @@
 #include "../dataobj/translator.h"
 #include "../dataobj/environment.h"
 #include "../obj/leitung2.h"
-#include "../display/viewport.h"
 #include "../utils/cbuffer_t.h"
 #include "../utils/simstring.h"
 #include "../tpl/array2d_tpl.h"
@@ -26,129 +25,8 @@
 
 #define BUTTONS_PER_ROW 4
 
-#define PAX_DEST_MIN_SIZE (16)      ///< minimum width/height of the minimap
-#define PAX_DEST_VERTICAL (4.0/3.0) ///< aspect factor where minimaps change to over/under instead of left/right
 
 
-/**
- * Component to show both passenger destination minimaps
- */
-class gui_city_minimap_t : public gui_world_component_t
-{
-	stadt_t* city;
-	scr_size minimaps_size;        ///< size of minimaps
-	scr_coord minimap2_offset;     ///< position offset of second minimap
-	array2d_tpl<PIXVAL> pax_dest_old, pax_dest_new;
-	uint32 pax_destinations_last_change;
-
-	void init_pax_dest( array2d_tpl<PIXVAL> &pax_dest );
-	void add_pax_dest( array2d_tpl<PIXVAL> &pax_dest, const sparse_tpl<PIXVAL>* city_pax_dest );
-
-public:
-	gui_city_minimap_t(stadt_t* city) : city(city),
-		pax_dest_old(0,0),
-		pax_dest_new(0,0)
-	{
-		minimaps_size = scr_size(PAX_DESTINATIONS_SIZE, PAX_DESTINATIONS_SIZE); // default minimaps size
-		minimap2_offset = scr_coord(minimaps_size.w + D_H_SPACE, 0);
-	}
-	scr_size get_min_size() const OVERRIDE { return scr_size(PAX_DEST_MIN_SIZE*2 + D_H_SPACE, PAX_DEST_MIN_SIZE); }
-
-	// set size of minimap, decide for horizontal or vertical arrangement
-	void set_size(scr_size size) OVERRIDE
-	{
-		// calculate new minimaps size : expand horizontally or vertically ?
-		const float world_aspect = (float)welt->get_size().x / (float)welt->get_size().y;
-		const scr_coord space(size.w, size.h);
-		const float space_aspect = (float)space.x / (float)space.y;
-
-		if(  world_aspect/space_aspect > PAX_DEST_VERTICAL  ) { // world wider than space, use vertical minimap layout
-			minimaps_size.h = (space.y - D_V_SPACE) / 2;
-			if(  minimaps_size.h * world_aspect <= space.x) {
-				// minimap fits
-				minimaps_size.w = (sint16) (minimaps_size.h * world_aspect);
-			}
-			else {
-				// too large, truncate
-				minimaps_size.w = space.x;
-				minimaps_size.h = max((int)(minimaps_size.w / world_aspect), PAX_DEST_MIN_SIZE);
-			}
-			minimap2_offset = scr_coord( 0, minimaps_size.h + D_V_SPACE );
-		}
-		else { // horizontal minimap layout
-			minimaps_size.w = (space.x - D_H_SPACE) / 2;
-			if (minimaps_size.w / world_aspect <= space.y) {
-				// minimap fits
-				minimaps_size.h = (sint16) (minimaps_size.w / world_aspect);
-			}
-			else {
-				// too large, truncate
-				minimaps_size.h = space.y;
-				minimaps_size.w = max((int)(minimaps_size.h * world_aspect), PAX_DEST_MIN_SIZE);
-			}
-			minimap2_offset = scr_coord( minimaps_size.w + D_H_SPACE, 0 );
-		}
-
-		// resize minimaps
-		pax_dest_old.resize( minimaps_size.w, minimaps_size.h );
-		pax_dest_new.resize( minimaps_size.w, minimaps_size.h );
-
-		// reinit minimaps data
-		init_pax_dest( pax_dest_old );
-		pax_dest_new = pax_dest_old;
-		add_pax_dest( pax_dest_old, city->get_pax_destinations_old() );
-		add_pax_dest( pax_dest_new, city->get_pax_destinations_new() );
-		pax_destinations_last_change = city->get_pax_destinations_new_change();
-
-		gui_world_component_t::set_size(scr_size(min(size.w, minimap2_offset.x + minimaps_size.w),min(size.h, minimap2_offset.y + minimaps_size.h)));
-	}
-	// handle clicks into minimaps
-	bool infowin_event(const event_t *ev) OVERRIDE
-	{
-		int my = ev->my;
-		if(  my > minimaps_size.h  &&  minimap2_offset.y > 0  ) {
-			// Little trick to handle both maps with the same code: Just remap the y-values of the bottom map.
-			my -= minimaps_size.h + D_V_SPACE;
-		}
-
-		if(  ev->ev_class!=EVENT_KEYBOARD  &&  ev->ev_code==MOUSE_LEFTBUTTON  &&  0<=my  &&  my<minimaps_size.h  ) {
-			int mx = ev->mx;
-			if(  mx > minimaps_size.w  &&  minimap2_offset.x > 0  ) {
-				// Little trick to handle both maps with the same code: Just remap the x-values of the right map.
-				mx -= minimaps_size.w + D_H_SPACE;
-			}
-
-			if(  0 <= mx && mx < minimaps_size.w  ) {
-				// Clicked in a minimap.
-				const koord p = koord(
-					(mx * welt->get_size().x) / (minimaps_size.w),
-									  (my * welt->get_size().y) / (minimaps_size.h));
-				welt->get_viewport()->change_world_position( p );
-				return true;
-			}
-		}
-		return false;
-	}
-	// draw both minimaps
-	void draw(scr_coord offset) OVERRIDE
-	{
-		const uint32 current_pax_destinations = city->get_pax_destinations_new_change();
-		if(  pax_destinations_last_change > current_pax_destinations  ) {
-			// new month started
-			pax_dest_old = pax_dest_new;
-			init_pax_dest( pax_dest_new );
-			add_pax_dest( pax_dest_new, city->get_pax_destinations_new());
-		}
-		else if(  pax_destinations_last_change != current_pax_destinations  ) {
-			// Since there are only new colors, this is enough:
-			add_pax_dest( pax_dest_new, city->get_pax_destinations_new() );
-		}
-		pax_destinations_last_change = current_pax_destinations;
-
-		display_array_wh(pos.x + offset.x, pos.y + offset.y, minimaps_size.w, minimaps_size.h, pax_dest_old.to_array() );
-		display_array_wh(pos.x + offset.x + minimap2_offset.x, pos.y + offset.y + minimap2_offset.y, minimaps_size.w, minimaps_size.h, pax_dest_new.to_array() );
-	}
-};
 
 
 const char *hist_type[MAX_CITY_HISTORY] =
@@ -195,7 +73,8 @@ const uint8 hist_type_color[MAX_CITY_HISTORY] =
 city_info_t::city_info_t(stadt_t* city) :
 	gui_frame_t( name, NULL ),
 	city(city),
-	scrolly_stats(&cont_city_stats, true)
+	scrolly_stats(&cont_city_stats, true),
+	pax_map(city)
 {
 	if (city) {
 		init();
@@ -212,7 +91,7 @@ void city_info_t::init()
 	name_input.add_listener( this );
 	add_component(&name_input);
 
-	add_table(2,0)->set_alignment(ALIGN_TOP);
+	add_table(1,0)->set_alignment(ALIGN_TOP);
 	{
 		add_table(1,0)->set_spacing(scr_size(D_H_SPACE, 0));
 		add_component(&lb_size);
@@ -231,8 +110,6 @@ void city_info_t::init()
 		allow_growth.add_listener( this );
 		add_component(&allow_growth);
 		end_table();
-
-		pax_map = new_component<gui_city_minimap_t>(city);
 	}
 	end_table();
 
@@ -242,8 +119,9 @@ void city_info_t::init()
 	year_month_tabs.add_tab(&container_month, translator::translate("Months"));
 	container_chart.add_component(&year_month_tabs);
 
-	tabs.add_tab(&container_chart, translator::translate("Chart"));
 	tabs.add_tab(&scrolly_stats, translator::translate("Statistics"));
+	tabs.add_tab(&container_chart, translator::translate("Chart"));
+	tabs.add_tab(&cont_destination_map, translator::translate("pax_destination_mapping"));
 	add_component(&tabs);
 	// .. put the same buttons in both containers
 	button_t* buttons[MAX_CITY_HISTORY];
@@ -303,6 +181,11 @@ void city_info_t::init()
 	transportation_last_year.add_color_value(&transportation_pas[3], color_idx_to_rgb(hist_type_color[HIST_PAS_TRANSPORTED]));
 	transportation_last_year.add_color_value(&transportation_pas[4], color_idx_to_rgb(hist_type_color[HIST_CITYCARS]));
 	transportation_last_year.add_color_value(&transportation_pas[5], color_idx_to_rgb(hist_type_color[HIST_PAS_WALKED]));
+
+	// passenger destination mapping
+	cont_destination_map.set_table_layout(1,0);
+	cont_destination_map.add_component(&pax_map);
+	cont_destination_map.new_component<gui_label_t>("Left:last month / Right:this month");
 
 	update_stats();
 
@@ -619,7 +502,7 @@ void city_info_t::draw(scr_coord pos, scr_size size)
 			update_stats();
 		}
 	}
-	const scr_coord_val margin_above_tab = name_input.get_pos().y + name_input.get_size().h + D_V_SPACE * 2 + max(allow_growth.get_pos().y + allow_growth.get_size().h, pax_map->get_pos().y + pax_map->get_size().h);
+	const scr_coord_val margin_above_tab = name_input.get_pos().y + name_input.get_size().h + D_V_SPACE * 2 + allow_growth.get_pos().y + allow_growth.get_size().h;
 	tabs.set_pos(scr_coord(0, margin_above_tab));
 	tabs.set_size(scr_size(tabs.get_size().w, get_client_windowsize().h-margin_above_tab));
 	gui_frame_t::draw(pos, size);
@@ -645,7 +528,7 @@ bool city_info_t::action_triggered( gui_action_creator_t *comp,value_t /* */)
 
 void city_info_t::map_rotate90( sint16 )
 {
-	pax_map->set_size( pax_map->get_size() );
+	pax_map.set_size( pax_map.get_size() );
 }
 
 
@@ -684,6 +567,7 @@ void city_info_t::rdwr(loadsave_t *file)
 
 	if(  file->is_loading()  ) {
 		city = welt->get_cities()[townindex];
+		pax_map.set_city(city);
 
 		init();
 		win_set_magic( this, (ptrdiff_t)city );
