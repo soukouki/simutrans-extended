b2c5194963630b379df62ce742fc44746444bb10
Markus Pristovsek <prissi@physik.tu-berlin.de>
2021-09-03 05:43:36 +0000

    ADD: easy date format settings from option window
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10068
    8aca7d54-2c30-db11-9de9-000461428c89
    
    # Conflicts:
    #	gui/display_settings.cc
    #	gui/display_settings.h
    #	simintr.cc

--- gui/display_settings.h
+++ gui/display_settings.h
@@ -44,21 +44,22 @@ public:
 	button_t reselect_closes_tool;
 
 	gui_settings_t();
-	virtual void draw( scr_coord offset ) OVERRIDE;
+	void draw( scr_coord offset ) OVERRIDE;
 };
 
 class map_settings_t : public gui_aligned_container_t, public action_listener_t
 {
 private:
+	char time_str[8][64];
 	gui_numberinput_t cursor_hide_range;
-	gui_combobox_t hide_buildings;
+	gui_combobox_t time_setting, hide_buildings;
 public:
 	gui_numberinput_t
 		inp_underground_level,
 		brightness,
 		scrollspeed;
 	map_settings_t();
-	virtual bool action_triggered( gui_action_creator_t *comp, value_t v ) OVERRIDE;
+	bool action_triggered( gui_action_creator_t *comp, value_t v ) OVERRIDE;
 	void draw(scr_coord offset) OVERRIDE;
 };
 
@@ -69,7 +70,7 @@ private:
 	button_t bt_convoy_id_plate[2];
 public:
 	label_settings_t();
-	virtual bool action_triggered(gui_action_creator_t *comp, value_t v);
+	bool action_triggered(gui_action_creator_t *comp, value_t v) OVERRIDE;
 	void draw(scr_coord offset) OVERRIDE;
 };
 
