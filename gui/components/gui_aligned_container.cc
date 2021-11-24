simutrans/simutrans

Only warn about lack of window space in DEBUG mode
This significantly reduces the amount of log messages for headless servers in non-DEBUG mode with -debug 2

git-svn-id: svn://localhost/simutrans/trunk@10184 8aca7d54-2c30-db11-9de9-000461428c89

gui/components/gui_aligned_container.cc
@@ -61,9 +61,11 @@ void gui_aligned_container_t::set_size(scr_size new_size)
 //	}
 //	printf("\n");
 
-	if (min_size.h > new_size.h  ||  min_size.w > new_size.w) {
-		dbg->warning("gui_aligned_container_t::set_size", "new size (%d,%d) smaller than min size (%d,%d)", new_size.w, new_size.h, min_size.w, min_size.h);
+#ifdef DEBUG
+	if (new_size.w < min_size.w  ||  new_size.h < min_size.h) {
+		dbg->warning("gui_aligned_container_t::set_size", "new size (%d,%d) cannot fit all elements; at least (%d,%d) required", new_size.w, new_size.h, min_size.w, min_size.h);
 	}
+#endif
 
 	scr_size extra = new_size - min_size;
 	extra.clip_lefttop(scr_coord(0,0));

soukouki/simutrans-extended

This significantly reduces the amount of log messages for headless
servers in non-DEBUG mode with -debug 2
git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10184
8aca7d54-2c30-db11-9de9-000461428c89
# Conflicts:
#	gui/components/gui_aligned_container.cc

gui/components/gui_aligned_container.cc
@@ -61,9 +61,11 @@ void gui_aligned_container_t::set_size(scr_size new_size)
 //	}
 //	printf("\n");
 
-	if (min_size.h > new_size.h  ||  min_size.w > new_size.w) {
-		dbg->message("gui_aligned_container_t::set_size", "new size (%d,%d) smaller than min size (%d,%d)", new_size.w, new_size.h, min_size.w, min_size.h);
+#ifdef DEBUG
+	if (new_size.w < min_size.w  ||  new_size.h < min_size.h) {
+		dbg->message("gui_aligned_container_t::set_size", "new size (%d,%d) cannot fit all elements; at least (%d,%d) required", new_size.w, new_size.h, min_size.w, min_size.h);
 	}
+#endif
 
 	scr_size extra = new_size - min_size;
 	extra.clip_lefttop(scr_coord(0,0));