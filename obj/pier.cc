8d3498bbd396929cb999818a9057a8b3dbbe0266
PJMack <pjmack314159@gmail.com>
2022-01-06 18:58:52 -0500

    fixed cherry-pick error

--- obj/pier.cc
+++ obj/pier.cc
@@ -433,7 +433,7 @@ void parapet_t::rotate90(){
 }
 
 const char *parapet_t::is_deletable(const player_t *player){
-	if (get_player_nr()==welt->get_public_player()->get_player_nr()) {
+	if (get_owner_nr()==welt->get_public_player()->get_player_nr()) {
 		return NULL;
 	}
 	return obj_t::is_deletable(player);
