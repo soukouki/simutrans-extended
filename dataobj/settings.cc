94175c618d4af2f6f4230d3153f3423db7c435a6
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2022-01-16 06:33:41 +0900

    FIX: Fix for max_speed_drive_by_sight_tram not being saved correctly was not complete

--- dataobj/settings.cc
+++ dataobj/settings.cc
@@ -1731,8 +1731,7 @@ void settings_t::rdwr(loadsave_t *file)
 				max_speed_drive_by_sight = kmh_to_speed(max_speed_drive_by_sight_kmh);
 			}
 #endif
-			if ((file->get_extended_version() == 14 && file->get_extended_revision() >= 31) || file->get_extended_version() >= 15)
-			{
+			if (file->is_version_ex_atleast(14, 46)) {
 				file->rdwr_long(max_speed_drive_by_sight_tram);
 				max_speed_drive_by_sight_tram = kmh_to_speed(max_speed_drive_by_sight_tram);
 			}
--- simversion.h
+++ simversion.h
@@ -35,7 +35,7 @@ extern "C" FILE * __cdecl __iob_func(void) { return _iob; }
 
 #define EX_VERSION_MAJOR	14
 #define EX_VERSION_MINOR	16
-#define EX_SAVE_MINOR		45
+#define EX_SAVE_MINOR		46
 
 // Do not forget to increment the save game versions in settings_stats.cc when changing this
 
