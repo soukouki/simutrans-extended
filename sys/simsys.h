d74aeaa8ed5b9af803dd4318ec6c15216187a3ee
ceeac <git.ceeac@gmail.com>
2022-01-21 20:10:35 +0000

    CODE: Use scr_size for dr_os_open
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10397
    8aca7d54-2c30-db11-9de9-000461428c89
    
    # Conflicts:
    #	sys/simsys_s.cc

--- sys/simsys.h
+++ sys/simsys.h
@@ -94,7 +94,7 @@ struct resolution
 };
 resolution dr_query_screen_resolution();
 
-int dr_os_open(int w, int h, sint16 fullscreen);
+int dr_os_open(scr_size window_size, sint16 fullscreen);
 void dr_os_close();
 
 // returns the locale; NULL if unknown
