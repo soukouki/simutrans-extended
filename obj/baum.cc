aburch/simutrans

FIX: offest of trees in networkgames no longer depends on the internal pointer to avoid desyncs (thanks for ceeac to find this)
git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9954 8aca7d54-2c30-db11-9de9-000461428c89

obj/baum.cc
@@ -261,11 +261,11 @@ void baum_t::recalc_off()
 // takes care of slopes
 void baum_t::calc_off(uint8 slope, sint8 x_, sint8 y_)
 {
-	const sint16 random = (sint16)( get_pos().x + get_pos().y + get_pos().z + slope + (sint16)(intptr_t)this );
+	const sint16 random = (sint16)( get_pos().x + get_pos().y + get_pos().z + slope + tree_id + geburt );
 
 	// point on tile (imaginary origin at sw corner, x axis: north, y axis: east
 	sint16 x = x_==-128 ? (random + tree_id) & 31  : x_;
-	sint16 y = y_==-128 ? (random + get_age()) & 31 : y_;
+	sint16 y = y_==-128 ? (random + geburt) & 31 : y_;
 
 	// the last bit has to be the same
 	y ^= x&1;

soukouki/simutrans-extended

git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9954
8aca7d54-2c30-db11-9de9-000461428c89
# Conflicts:
#	obj/baum.cc

obj/baum.cc
@@ -325,10 +325,11 @@ bool baum_t::register_desc(tree_desc_t *desc)
 // takes care of slopes
 void baum_t::calc_off(uint8 slope, sint8 x_, sint8 y_)
 {
-	sint16 random = (sint16)( get_pos().x + get_pos().y + get_pos().z + slope + (uint64)this );
+	const sint16 random = (sint16)( get_pos().x + get_pos().y + get_pos().z + slope + tree_id + purchase_time );
+	
 	// point on tile (imaginary origin at sw corner, x axis: north, y axis: east
 	sint16 x = x_==-128 ? (random + tree_id) & 31  : x_;
-	sint16 y = y_==-128 ? (random + get_age()) & 31 : y_;
+	sint16 y = y_==-128 ? (random + purchase_time) & 31 : y_;
 
 	// the last bit has to be the same
 	y = y ^ (x&1);