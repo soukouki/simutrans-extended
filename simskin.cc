9a10d0ca0731532b8b3c5e96036953999d7b220e
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2022-03-29 01:39:19 +0900

    CHG: Organize icon categories

--- simskin.cc
+++ simskin.cc
@@ -125,25 +125,22 @@ static special_obj_tpl<skin_desc_t> const menu_objekte[] = {
 	{ &skinverwaltung_t::tool_icons_simple,  "SimpleTools"  },
 	{ &skinverwaltung_t::tool_icons_dialoge, "DialogeTools" },
 	{ &skinverwaltung_t::tool_icons_toolbars,"BarTools"     },
+
+	{ &skinverwaltung_t::alerts,             "Alerts"         },
+	{ &skinverwaltung_t::waiting_time,       "WaitingTime"    },
+	{ &skinverwaltung_t::travel_time,        "TravelTime"     },
+	{ &skinverwaltung_t::service_frequency,  "ServiceFrequency" },
+	{ &skinverwaltung_t::missing_scheduled_slot, "MissingScheduledSlot" },
+	{ &skinverwaltung_t::on_foot,            "OnFoot"         },
+	{ &skinverwaltung_t::upgradable,         "Upgradable"     },
+	{ &skinverwaltung_t::input_output,       "InputOutput"    },
+	{ &skinverwaltung_t::in_transit,         "InTransit"      },
+	{ &skinverwaltung_t::reverse_arrows,     "ReverseArrows"  },
+	{ &skinverwaltung_t::open_window,        "OpenWindow"     },
 	{ NULL, NULL }
 };
 
 static special_obj_tpl<skin_desc_t> const symbol_objekte[] = {
-	{ &skinverwaltung_t::missing_scheduled_slot, "MissingScheduledSlot" },
-	{ &skinverwaltung_t::upgradable,         "Upgradable"     },
-	{ &skinverwaltung_t::pax_evaluation_icons, "PassengersEvaluation" },
-	{ &skinverwaltung_t::mail_evaluation_icons, "MailEvaluation" },
-	{ &skinverwaltung_t::alerts,             "Alerts"         },
-	{ &skinverwaltung_t::goods_categories,   "GoodsCategories"},
-	{ &skinverwaltung_t::input_output,       "InputOutput"    },
-	{ &skinverwaltung_t::travel_time,        "TravelTime"     },
-	{ &skinverwaltung_t::in_transit,         "InTransit"      },
-	{ &skinverwaltung_t::ind_sector_symbol,  "IndustrySectors" },
-	{ &skinverwaltung_t::reverse_arrows,     "ReverseArrows"  },
-	{ &skinverwaltung_t::waiting_time,       "WaitingTime"    },
-	{ &skinverwaltung_t::service_frequency,  "ServiceFrequency" },
-	{ &skinverwaltung_t::on_foot,            "OnFoot"         },
-	{ &skinverwaltung_t::open_window,        "OpenWindow"     },
 	{ &skinverwaltung_t::seasons_icons,      "Seasons"        },
 	{ &skinverwaltung_t::message_options,    "MessageOptions" },
 	{ &skinverwaltung_t::color_options,      "ColorOptions"   },
@@ -178,9 +175,14 @@ static special_obj_tpl<skin_desc_t> const fakultative_objekte[] = {
 	{ &skinverwaltung_t::fastforwardsymbol,  "fastforwardsym" },
 	{ &skinverwaltung_t::pausesymbol,        "pausesym"       },
 	{ &skinverwaltung_t::station_type,       "station_type"   },
-	{ &skinverwaltung_t::toolbar_background,"ToolsBackground"},
+	{ &skinverwaltung_t::toolbar_background, "ToolsBackground"},
 	{ &skinverwaltung_t::compass_iso,        "CompassIso"     },
 	{ &skinverwaltung_t::compass_map,        "CompassMap"    },
+	{ &skinverwaltung_t::pax_evaluation_icons, "PassengersEvaluation" },
+	{ &skinverwaltung_t::mail_evaluation_icons, "MailEvaluation" },
+	{ &skinverwaltung_t::goods_categories,   "GoodsCategories"},
+	{ &skinverwaltung_t::ind_sector_symbol,  "IndustrySectors" },
+	{ &skinverwaltung_t::minimap,            "Minimap"        },
 	{ NULL, NULL }
 };
 
@@ -198,8 +200,8 @@ bool skinverwaltung_t::successfully_loaded(skintyp_t type)
 	special_obj_tpl<skin_desc_t> const* sd;
 	switch (type) {
 		case menu:    return true; // skins will be handled elsewhere
-		case cursor:  sd = cursor_objekte;     break;
-		case symbol:  sd = symbol_objekte+15;  break;
+		case cursor:  sd = cursor_objekte; break;
+		case symbol:  sd = symbol_objekte; break;
 		case misc:
 			sd = misc_objekte+3;
 			// for compatibility: use sidewalk as tunneltexture
