soukouki/simutrans-extended

commit 54c860f33608bc6ee7711764b3657b46d7d8c8d1

FIX: Network filter comboboxes take preference capturing the mouse wh… …eel over the minimap
git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10564 8aca7d54-2c30-db11-9de9-000461428c89
Co-Authored-By: Roboron3042 <robertoms258@gmail.com>

gui/components/gui_combobox.h
@@ -163,6 +163,14 @@ public:
 
 	scr_size get_max_size() const OVERRIDE;
 
+	/**
+	 * Checks if the given position is inside the component area, including the droplist.
+	 * @return true if the coordinates are inside this component area.
+	 */
+	bool getroffen(int x, int y) OVERRIDE {
+		return ( pos.x <= x && pos.y <= y && (pos.x+size.w+droplist.get_size().w+1) > x && (pos.y+size.h+droplist.get_size().h+1) > y );
+	}
+
 	void enable();
 	void disable();
 

gui/map_frame.cc
@@ -697,8 +697,7 @@ void map_frame_t::zoom(bool magnify)
 
 
 /**
- * Events werden hiermit an die GUI-components
- * gemeldet
+ * Report events to the GUI-components
  */
 bool map_frame_t::infowin_event(const event_t *ev)
 {
@@ -718,8 +717,13 @@ bool map_frame_t::infowin_event(const event_t *ev)
 		set_focus( minimap_t::get_instance() );
 	}
 
-	if(  (IS_WHEELUP(ev) || IS_WHEELDOWN(ev))  &&  minimap_t::get_instance()->getroffen(ev2.mx,ev2.my)  ) {
-		// otherwise these would go to the vertical scroll bar
+	if(  (IS_WHEELUP(ev) || IS_WHEELDOWN(ev))  &&  
+		minimap_t::get_instance()->getroffen(ev2.mx,ev2.my)  && 
+		!viewed_player_c.getroffen(ev2.mx,ev2.my) &&
+		!transport_type_c.getroffen(ev2.mx,ev2.my) &&
+		!freight_type_c.getroffen(ev2.mx,ev2.my)  )
+	{
+		// otherwise these would go to the vertical scroll bars
 		zoom(IS_WHEELUP(ev));
 		return true;
 	}

commit 35f669056cd4a5e9ed94421bf3602946af32c77d

FIX: The behavior of the GUI where the combobox is placed is somewhat broken
Partially reverts the code introduced by r10564.

gui/components/gui_combobox.h
@@ -163,14 +163,6 @@ public:
 
 	scr_size get_max_size() const OVERRIDE;
 
-	/**
-	 * Checks if the given position is inside the component area, including the droplist.
-	 * @return true if the coordinates are inside this component area.
-	 */
-	bool getroffen(int x, int y) OVERRIDE {
-		return ( pos.x <= x && pos.y <= y && (pos.x+size.w+droplist.get_size().w+1) > x && (pos.y+size.h+droplist.get_size().h+1) > y );
-	}
-
 	void enable();
 	void disable();
 