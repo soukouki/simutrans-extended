0b1b6a8a0d70af404ab8fbdce0ea958352ed9b10
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2022-05-29 00:05:29 +0900

    CHG: Execution order optimization

--- gui/components/gui_vehicle_capacitybar.cc
+++ gui/components/gui_vehicle_capacitybar.cc
@@ -38,11 +38,11 @@ void gui_convoy_loading_info_t::update_list()
 
 				bool is_lowest_class = true; // for display catgroy symbol
 				for (uint8 i = 0; i < g_classes; i++) {
-					const goods_desc_t* ware = goods_manager_t::get_info_catg_index(catg_index);
 					const uint16 capacity = get_unique_fare_capacity(catg_index,i);
 					if (!capacity) {
 						continue;
 					}
+					const goods_desc_t* ware = goods_manager_t::get_info_catg_index(catg_index);
 					// 1: goods category symbol
 					if (is_lowest_class) {
 						new_component<gui_image_t>(ware->get_catg_symbol(), 0, ALIGN_CENTER_V, true);
