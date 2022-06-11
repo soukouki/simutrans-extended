893ce9a8b33e97ee7d562c3d3746820a7345d7d4
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2022-06-12 01:10:52 +0900

    :arrow_upper_right: CHG/CODE: The design of the convoy / line button is not a coordinate button but a button that opens a separate window (if there is a suitable image)

--- gui/components/gui_convoi_button.h
+++ gui/components/gui_convoi_button.h
@@ -11,6 +11,10 @@
 #include "action_listener.h"
 
 #include "../../simconvoi.h"
+#include "../../simline.h"
+#include "../../player/simplay.h"
+#include "../../simlinemgmt.h"
+
 
 /**
  * Button to open convoi window
@@ -22,12 +26,44 @@ class gui_convoi_button_t : public button_t, public action_listener_t
 public:
 gui_convoi_button_t(convoihandle_t convoi) : button_t() {
 		this->convoi = convoi;
-		init(button_t::posbutton, NULL);
+		if( skinverwaltung_t::open_window ) {
+			init(button_t::imagebox_state, NULL);
+			set_image(skinverwaltung_t::open_window ? skinverwaltung_t::open_window->get_image_id(0) : skinverwaltung_t::goods->get_image_id(0));
+		}
+		else {
+			init(button_t::posbutton, NULL);
+		}
 		add_listener(this);
 	}
 
 	bool action_triggered(gui_action_creator_t*, value_t) OVERRIDE {
 		convoi->show_info();
+		return true;
+	}
+};
+
+/**
+ * Button to open line window
+ */
+class gui_line_button_t : public button_t, public action_listener_t
+{
+	linehandle_t line;
+
+public:
+	gui_line_button_t(linehandle_t line) : button_t() {
+		this->line = line;
+		if( skinverwaltung_t::open_window ) {
+			init(button_t::imagebox_state, NULL);
+			set_image(skinverwaltung_t::open_window ? skinverwaltung_t::open_window->get_image_id(0) : skinverwaltung_t::goods->get_image_id(0));
+		}
+		else {
+			init(button_t::posbutton, NULL);
+		}
+		add_listener(this);
+	}
+
+	bool action_triggered(gui_action_creator_t*, value_t) OVERRIDE {
+		line->get_owner()->simlinemgmt.show_lineinfo(line->get_owner(), line);
 		return true;
 	}
 };
