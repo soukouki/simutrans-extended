755a266f28e0d9854d858eb38d04c00a9d723316
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2022-07-07 23:52:13 +0900

    FIX: Reloading with the depot dialog open may cause a crash

--- gui/depot_frame.cc
+++ gui/depot_frame.cc
@@ -921,7 +921,6 @@ void depot_frame_t::rdwr(loadsave_t *file)
 	simline_t::rdwr_linehandle_t(file, selected_line);
 
 	if(  depot  &&  file->is_loading()  ) {
-		convoy_assembler.update_convoi();
 		reset_min_windowsize();
 		set_windowsize(size);
 
