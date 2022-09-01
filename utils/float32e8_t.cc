a453ba75beeda813de166fe32f157ea5e5543a5b
ceeac <ceeac@users.noreply.github.com>
2022-09-01 09:48:32 +0200

    Remove using namespace std from float32e8_t.h

--- utils/float32e8_t.cc
+++ utils/float32e8_t.cc
@@ -5,16 +5,20 @@
  *      Author: Bernd Gabriel
  */
 
-#include <stdlib.h>
 #include "float32e8_t.h"
+
 #include "../simdebug.h"
 #include "../dataobj/loadsave.h"
 
-ostream & operator << (ostream &out, const float32e8_t &x)
+#include <cstdlib>
+
+
+std::ostream & operator << (std::ostream &out, const float32e8_t &x)
 {
 	out << x.to_double();
 	return out;
 }
+
 
 //remember: most significant bit is always set except for 0.
 //after any operation the mantissa is shifted and the exponent is in-/decremented accordingly.
