0b8da660a295e639b5b483b5b5eb579c187f73d5
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2022-10-22 19:30:37 +0900

    FIX: signal indicator not working if ribi_arrow is missing from pakset

--- boden/wege/strasse.h
+++ boden/wege/strasse.h
@@ -70,7 +70,7 @@ public:
 
 	image_id get_front_image() const OVERRIDE
 	{
-		if (show_masked_ribi && overtaking_mode <= oneway_mode) {
+		if (skinverwaltung_t::ribi_arrow && show_masked_ribi && overtaking_mode <= oneway_mode) {
 			return skinverwaltung_t::ribi_arrow->get_image_id(get_ribi());
 		}
 		else {
