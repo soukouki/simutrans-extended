soukouki/simutrans-extended

commit eccdc115323fb99dc360d77970585c2571b1ee16

ADD: New factory placement locations: river_city and shore_city. This means that the factory must both be in a city and on a river/shore. NOTE: It can be difficult for the game to find a suitable location for these industries with the current algorithm.

gui/factory_edit.cc
@@ -184,8 +184,8 @@ void factory_edit_frame_t::fill_list()
 				&&  ( desc->get_building()->get_allowed_climate_bits() & get_climate()) ) {
 				// timeline allows for this, and so does climates setting
 
-				if( ( city_chain  &&  (desc->get_placement() == factory_desc_t::City && desc->is_consumer_only() ) )
-				||  ( land_chain  &&  (desc->get_placement() != factory_desc_t::City && desc->is_consumer_only() ) )
+				if( ( city_chain  &&  ((desc->get_placement() == factory_desc_t::City || desc->get_placement() == factory_desc_t::shore_city || desc->get_placement() == factory_desc_t::river_city) && desc->is_consumer_only() ) )
+				||  ( land_chain  &&  ((desc->get_placement() == factory_desc_t::City || desc->get_placement() == factory_desc_t::shore_city || desc->get_placement() == factory_desc_t::river_city) && desc->is_consumer_only() ) )
 				||  (!city_chain  &&  !land_chain) ) {
 					switch(sortedby) {
 						case gui_sorting_item_t::BY_NAME_TRANSLATED:     factory_list.insert_ordered( desc, compare_factory_desc_name );           break;

Simutrans-Extended.vcxproj
@@ -1943,6 +1943,7 @@
       <ExcludedFromBuild Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">true</ExcludedFromBuild>
       <ExcludedFromBuild Condition="'$(Configuration)|$(Platform)'=='Debug|x64'">true</ExcludedFromBuild>
       <ExcludedFromBuild Condition="'$(Configuration)|$(Platform)'=='Debug (non-graphical server)|x64'">true</ExcludedFromBuild>
+      <ExcludedFromBuild Condition="'$(Configuration)|$(Platform)'=='Optimised debug|x64'">true</ExcludedFromBuild>
     </ClCompile>
     <ClCompile Include="sys\clipboard_w32.cc" />
     <ClCompile Include="dataobj\environment.cc" />

bauer/fabrikbauer.cc
@@ -175,13 +175,33 @@ public:
 								condmet += gr->hat_weg(road_wt);
 								break;
 							case factory_desc_t::shore:
-								condmet += welt->get_climate(k)==water_climate;
+							case factory_desc_t::shore_city:
+								if (welt->get_climate(k) == water_climate)
+								{
+									if (site == factory_desc_t::shore_city)
+									{
+										condmet += gr->hat_weg(road_wt);
+									}
+									else
+									{
+										condmet++;
+									}
+								}
 								break;
 							case factory_desc_t::river:
+							case factory_desc_t::river_city:
 							if(  welt->get_settings().get_river_number() >0  ) {
 								weg_t* river = gr->get_weg(water_wt);
-								if (river  &&  river->get_desc()->get_styp()==type_river) {
-									condmet++;
+								if (river  &&  river->get_desc()->get_styp()==type_river)
+								{
+									if (site == factory_desc_t::river_city)
+									{
+										condmet += gr->hat_weg(road_wt);
+									}
+									else
+									{
+										condmet++;
+									}
 									DBG_DEBUG("factory_site_searcher_t::is_area_ok()", "Found river near %s", pos.get_str());
 								}
 								break;
@@ -647,7 +667,7 @@ int factory_builder_t::build_link(koord3d* parent, const factory_desc_t* info, s
 	}
 
 	// no cities at all?
-	if (info->get_placement() == factory_desc_t::City  &&  welt->get_cities().empty()) {
+	if ((info->get_placement() == factory_desc_t::City || info->get_placement() == factory_desc_t::shore_city || info->get_placement() == factory_desc_t::river_city) &&  welt->get_cities().empty()) {
 		return 0;
 	}
 
@@ -662,7 +682,7 @@ int factory_builder_t::build_link(koord3d* parent, const factory_desc_t* info, s
 	}
 
 	// Industries in town needs different place search
-	if (info->get_placement() == factory_desc_t::City) {
+	if (info->get_placement() == factory_desc_t::City || info->get_placement() == factory_desc_t::shore_city || info->get_placement() == factory_desc_t::river_city) {
 
 		koord size=info->get_building()->get_size(0);
 
@@ -1306,7 +1326,7 @@ next_ware_check:
 						continue;
 					}
 				}
-				const bool in_city = consumer->get_placement() == factory_desc_t::City;
+				const bool in_city = consumer->get_placement() == factory_desc_t::City || consumer->get_placement() == factory_desc_t::shore_city || consumer->get_placement() == factory_desc_t::river_city;
 				if (in_city && welt->get_cities().empty())
 				{
 					// we cannot build this factory here

descriptor/factory_desc.h
@@ -197,7 +197,9 @@ public:
 		City,
 		river,
 		shore,
-		forest
+		forest,
+		river_city,
+		shore_city
 	};
 
 private:

descriptor/writer/factory_writer.cc
@@ -167,6 +167,8 @@ void factory_writer_t::write_obj(FILE* fp, obj_node_t& parent, tabfileobj_t& obj
 		!STRICMP(placing, "river") ? factory_desc_t::river  :
 		!STRICMP(placing, "shore") ? factory_desc_t::shore  :
 		!STRICMP(placing, "forest")? factory_desc_t::forest :
+		!STRICMP(placing, "river_city") ? factory_desc_t::river_city :
+		!STRICMP(placing, "shore_city") ? factory_desc_t::shore_city :
 		factory_desc_t::Land;
 	uint16 const productivity = obj.get_int("productivity",        10);
 	uint16 const range        = obj.get_int("range",               10);

commit 0af3a27536f5feb421aa4591bd66bd93142bb18a

FIX: rural chain filter on the factory edit dialog is broken
It happened with commit eccdc115323fb99dc360d77970585c2571b1ee16

gui/factory_edit.cc
@@ -188,7 +188,7 @@ void factory_edit_frame_t::fill_list()
 				// timeline allows for this, and so does climates setting
 
 				if( ( city_chain  &&  ((desc->get_placement() == factory_desc_t::City || desc->get_placement() == factory_desc_t::shore_city || desc->get_placement() == factory_desc_t::river_city) && desc->is_consumer_only() ) )
-				||  ( land_chain  &&  ((desc->get_placement() == factory_desc_t::City || desc->get_placement() == factory_desc_t::shore_city || desc->get_placement() == factory_desc_t::river_city) && desc->is_consumer_only() ) )
+				||  ( land_chain  &&  ((desc->get_placement() != factory_desc_t::City && desc->get_placement() != factory_desc_t::shore_city && desc->get_placement() != factory_desc_t::river_city) && desc->is_consumer_only() ) )
 				||  (!city_chain  &&  !land_chain) ) {
 					switch(sortedby) {
 						case gui_sorting_item_t::BY_NAME_TRANSLATED:     factory_list.insert_ordered( desc, compare_factory_desc_name );           break;