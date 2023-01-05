56c84a9c32241ddb75258adc0da961390bbfbd81
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2023-01-06 02:23:07 +0900

    Revert "UI design update: traffic lights cycle switcher"
    This reverts commit 8d59e219eeac1e98f87bde0997e1cec6ff7da8b6.

--- gui/trafficlight_info.cc
+++ gui/trafficlight_info.cc
@@ -6,7 +6,6 @@
 #include "trafficlight_info.h"
 #include "components/gui_label.h"
 #include "components/gui_colorbox.h"
-#include "components/gui_table.h"
 #include "../obj/roadsign.h" // The rest of the dialog
 
 #include "../simmenu.h"
@@ -16,63 +15,50 @@ trafficlight_info_t::trafficlight_info_t(roadsign_t* s) :
 	obj_infowin_t(s),
 	roadsign(s)
 {
-	add_table(3,1);
+	add_table(5,3)->set_alignment(ALIGN_CENTER_H);
 	{
-		gui_aligned_container_t *ticks_table = add_table(3,3);
-		{
-			ticks_table->set_alignment(ALIGN_CENTER_H | ALIGN_CENTER_V);
-			ticks_table->set_margin(scr_size(0, (LINESPACE>>1)-1), scr_size(0, 0));
-			ticks_table->set_table_frame(true, true);
-			new_component<gui_empty_t>();
-			new_component<gui_colorbox_t>(15911)->set_size(scr_size(LINESPACE-2, LINESPACE-2));
-			new_component<gui_colorbox_t>(65024)->set_size(scr_size(LINESPACE-2, LINESPACE-2));
-
-			new_component<gui_table_header_t>("east_and_west", SYSCOL_TH_BACKGROUND_LEFT, gui_label_t::left);
-			ns.set_limits( 1, 255 );
-			ns.set_value( s->get_ticks_ns() );
-			ns.wrap_mode( false );
-			ns.add_listener( this );
-			add_component( &ns );
-
-			amber_ns.set_limits(1, 255);
-			amber_ns.set_value(s->get_ticks_amber_ns());
-			amber_ns.wrap_mode(false);
-			amber_ns.add_listener(this);
-			add_component(&amber_ns);
-
-			new_component<gui_table_header_t>("north_and_south", SYSCOL_TH_BACKGROUND_LEFT, gui_label_t::left);
-			ow.set_limits( 1, 255 );
-			ow.set_value( s->get_ticks_ow() );
-			ow.wrap_mode( false );
-			ow.add_listener( this );
-			add_component( &ow );
-
-			amber_ow.set_limits( 1, 255 );
-			amber_ow.set_value( s->get_ticks_amber_ow() );
-			amber_ow.wrap_mode( false );
-			amber_ow.add_listener( this );
-			add_component( &amber_ow );
-		}
-		end_table();
-
+		new_component<gui_empty_t>();
+		new_component<gui_colorbox_t>(15911)->set_size(scr_size(LINESPACE-2, LINESPACE-2));
+		new_component<gui_colorbox_t>(65024)->set_size(scr_size(LINESPACE-2, LINESPACE-2));
 		new_component<gui_margin_t>(LINESPACE);
+		new_component<gui_label_t>("shift");
 
-		gui_aligned_container_t *offset_table = add_table(1,3);
-		{
-			offset_table->set_table_frame(true, true);
-			new_component<gui_table_header_t>("shift", SYSCOL_TH_BACKGROUND_LEFT);
-			offset.set_limits( 0, 255 );
-			offset.set_value( s->get_ticks_offset() );
-			offset.wrap_mode( false );
-			offset.add_listener( this );
-			add_component( &offset );
+		new_component<gui_label_t>("east_and_west");
+		ns.set_limits( 1, 255 );
+		ns.set_value( s->get_ticks_ns() );
+		ns.wrap_mode( false );
+		ns.add_listener( this );
+		add_component( &ns );
 
-			new_component<gui_margin_t>(1,LINESPACE);
-		}
-		end_table();
+		amber_ns.set_limits(1, 255);
+		amber_ns.set_value(s->get_ticks_amber_ns());
+		amber_ns.wrap_mode(false);
+		amber_ns.add_listener(this);
+		add_component(&amber_ns);
+
+		new_component<gui_empty_t>();
+
+		offset.set_limits( 0, 255 );
+		offset.set_value( s->get_ticks_offset() );
+		offset.wrap_mode( false );
+		offset.add_listener( this );
+		add_component( &offset );
+
+
+		new_component<gui_label_t>("north_and_south");
+		ow.set_limits( 1, 255 );
+		ow.set_value( s->get_ticks_ow() );
+		ow.wrap_mode( false );
+		ow.add_listener( this );
+		add_component( &ow );
+
+		amber_ow.set_limits( 1, 255 );
+		amber_ow.set_value( s->get_ticks_amber_ow() );
+		amber_ow.wrap_mode( false );
+		amber_ow.add_listener( this );
+		add_component( &amber_ow );
 	}
 	end_table();
-
 
 	// show author below the settings
 	if (char const* const maker = roadsign->get_desc()->get_copyright()) {
