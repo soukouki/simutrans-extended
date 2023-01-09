5a3f6a2add2eda2a5afd8b0b12cbfc7ec06fb031
Ranran-the-JuicyPork <Ranran-the-JuicyPork@users.noreply.github.com>
2023-01-09 22:04:47 +0000

    run ./cleanup_code.sh

--- gui/schedule_list.cc
+++ gui/schedule_list.cc
@@ -372,14 +372,14 @@ schedule_list_gui_t::schedule_list_gui_t(player_t *player_) :
 		bt_replace.set_visible(false);
 		bt_replace.add_listener(this);
 		cont_line_info.add_component(&bt_replace);
-			
+
 		bt_withdraw_line.init(button_t::box_state, "Withdraw All", scr_coord(0, 0), scr_size(D_BUTTON_WIDTH+18,D_BUTTON_HEIGHT));
 		bt_withdraw_line.set_tooltip("Convoi is sold when all wagons are empty.");
 		if (skinverwaltung_t::alerts) {
 			bt_withdraw_line.set_image(skinverwaltung_t::alerts->get_image_id(2));
 		}
 		bt_withdraw_line.add_listener(this);
-		
+
 		cont_line_info.new_component_span<gui_margin_t>(0,D_V_SPACE,2);
 
 		cont_line_info.add_component(&lb_convoy_count,2);
