6d711e68535eee42b3ce47fbc60b045841794364
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2023-01-13 06:12:47 +0900

    ADD functions to get the traction type bits

--- gui/components/gui_convoy_assembler.cc
+++ gui/components/gui_convoy_assembler.cc
@@ -1495,8 +1495,7 @@ void gui_convoy_assembler_t::build_vehicle_lists()
 						}
 					}
 				}
-				const uint16 shifter = 1 << info->get_engine_type();
-				const bool correct_traction_type = veh_action == va_sell || (replace_frame && shifter & replace_frame->get_traction_types() ) || (depot_frame && (shifter & depot_frame->get_depot()->get_traction_types()));
+				const bool correct_traction_type = veh_action == va_sell || (replace_frame && info->get_traction_type() & replace_frame->get_traction_types() ) || (depot_frame && (info->get_traction_type() & depot_frame->get_depot()->get_traction_types()));
 				const weg_t* way = depot_frame ? world()->lookup(depot_frame->get_depot()->get_pos())->get_weg(depot_frame->get_depot()->get_waytype()) : NULL;
 				const bool correct_way_constraint = !way || missing_way_constraints_t(info->get_way_constraints(), way->get_way_constraints()).check_next_tile();
 				if(!correct_way_constraint || (!correct_traction_type && (info->get_power() > 0 || (veh_action == va_insert && info->get_leader_count() == 1 && info->get_leader(0) && info->get_leader(0)->get_power() > 0))))
@@ -1892,9 +1891,7 @@ void gui_convoy_assembler_t::update_data()
 				}
 				if(depot_frame && (i.key->get_power() > 0 || (veh_action == va_insert && i.key->get_leader_count() == 1 && i.key->get_leader(0) && i.key->get_leader(0)->get_power() > 0)))
 				{
-					const uint16 traction_type = i.key->get_engine_type();
-					const uint16 shifter = 1 << traction_type;
-					if(!(shifter & depot_frame->get_depot()->get_traction_types()))
+					if(!(i.key->get_traction_type() & depot_frame->get_depot()->get_traction_types()))
 					{
 						// Do not allow purchasing of vehicle if depot is of the wrong type.
 						img.lcolor = COL_DANGER;
--- simconvoi.cc
+++ simconvoi.cc
@@ -6854,6 +6854,15 @@ bool convoi_t::has_same_vehicles(convoihandle_t other) const
 	return false;
 }
 
+uint16 convoi_t::get_traction_types() const
+{
+	uint16 traction_types = 0;
+	for (uint8 i = 0; i < vehicle_count; i++) {
+		traction_types |= vehicle[i]->get_desc()->get_traction_type();
+	}
+	return traction_types;
+}
+
 /*
  * Will find a depot for the vehicle "master".
  */
