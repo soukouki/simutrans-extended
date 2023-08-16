simutrans/simutrans

CODE: finish calculation early for convois with one vehicle
git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10235 8aca7d54-2c30-db11-9de9-000461428c89

simconvoi.cc
@@ -2894,16 +2894,19 @@ void convoi_t::hat_gehalten(halthandle_t halt)
 		// harbour and river stop load any size
 		vehicles_loading = anz_vehikel;
 	}
+	else if (anz_vehikel == 1  &&  CARUNITS_PER_TILE >= fahr[0]->get_desc()->get_length()) {
+		vehicles_loading = 1;
+		// one vehicle, which fits into one tile
+	}
 	else {
-		// calculate real station length
-		// and numbers of vehicles that can be (un)loaded
+		// difference between actual station length and vehicle lenghts
+		sint16 station_length = -fahr[vehicles_loading]->get_desc()->get_length();
+		// iterate through tiles in straight line and look for station
 		koord zv = koord( ribi_t::backward(fahr[0]->get_direction()) );
 		koord3d pos = fahr[0]->get_pos();
 		grund_t *gr=welt->lookup(pos);
 		// start on bridge?
 		pos.z += gr->get_weg_yoff() / TILE_HEIGHT_STEP;
-		// difference between actual station length and vehicle lenghts
-		sint16 station_length = -fahr[vehicles_loading]->get_desc()->get_length();
 		do {
 			// advance one station tile
 			station_length += CARUNITS_PER_TILE;

soukouki/simutrans-extended

git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10235
8aca7d54-2c30-db11-9de9-000461428c89
# Conflicts:
#	simconvoi.cc

simconvoi.cc
@@ -5756,16 +5756,18 @@ void convoi_t::hat_gehalten(halthandle_t halt)
 		// harbour has any size
 		vehicles_loading = vehicle_count;
 	}
-	else
-	{
-		// calculate real station length
-		// and numbers of vehicles that can be (un)loaded
+	else if (vehicle_count == 1  &&  CARUNITS_PER_TILE >= vehicle[0]->get_desc()->get_length()) {
+		vehicles_loading = 1;
+		// one vehicle, which fits into one tile
+	}
+	else {
+		// difference between actual station length and vehicle lenghts
+		sint16 station_length = -vehicle[vehicles_loading]->get_desc()->get_length();
+		// iterate through tiles in straight line and look for station
 		koord zv = koord( ribi_t::backward(front()->get_direction()) );
 		koord3d pos = front()->get_pos();
 		// start on bridge?
 		pos.z += gr->get_weg_yoff() / TILE_HEIGHT_STEP;
-		// difference between actual station length and vehicle lenghts
-		sint16 station_length = -vehicle[vehicles_loading]->get_desc()->get_length();
 		do {
 			// advance one station tile
 			station_length += CARUNITS_PER_TILE;