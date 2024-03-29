aburch/simutrans

Add tool to viually invert control
git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10590 8aca7d54-2c30-db11-9de9-000461428c89

src/simutrans/tool/simmenu.cc
@@ -47,6 +47,9 @@
 
 karte_ptr_t tool_t::welt;
 
+// emulate control key by tool
+uint8 tool_t::control_invert = 0;
+
 // for key lookup; is always sorted during the game
 vector_tpl<tool_t *>tool_t::char_to_tool(0);
 
@@ -322,6 +325,7 @@ tool_t *create_simple_tool(int toolnr)
 		case TOOL_ROLLUP_ALL_WIN:       tool = new tool_rollup_all_win_t();       break;
 		case TOOL_RECOLOUR_TOOL:        tool = new tool_recolour_t();             break;
 		case TOOL_SHOW_FACTORY_STORAGE: tool = new tool_show_factory_storage_t(); break;
+		case TOLL_TOGGLE_CONTROL:       tool = new tool_toggle_control_t();       break;
 		case UNUSED_TOOL_ADD_MESSAGE: // fall-through - intended!!!111elf
 		case UNUSED_WKZ_PWDHASH_TOOL:
 			dbg->warning("create_simple_tool()", "Deprecated tool [%i] requested", toolnr);

src/simutrans/tool/simtool.h
@@ -922,6 +922,23 @@ public:
 	bool is_work_network_safe() const OVERRIDE { return true; }
 };
 
+class tool_toggle_control_t : public tool_t {
+public:
+	tool_toggle_control_t() : tool_t(TOLL_TOGGLE_CONTROL | SIMPLE_TOOL) {}
+	char const* get_tooltip(player_t const*) const OVERRIDE { return translator::translate("Lock Control key"); }
+	bool is_selected() const OVERRIDE { return tool_t::control_invert; }
+	bool init(player_t*) OVERRIDE {
+		tool_t::control_invert = WFL_CTRL;
+		return false;
+	}
+	bool exit(player_t* s) OVERRIDE {
+		tool_t::control_invert = 0;
+		return false;
+	}
+	bool is_init_network_safe() const OVERRIDE { return true; }
+	bool is_work_network_safe() const OVERRIDE { return true; }
+};
+
 class tool_rotate90_t : public tool_t {
 public:
 	tool_rotate90_t() : tool_t(TOOL_ROTATE90 | SIMPLE_TOOL) {}

soukouki/simutrans-extended

git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10590
8aca7d54-2c30-db11-9de9-000461428c89
git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10597
git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10598
NOTE: Include fix of typo

simmenu.cc
@@ -47,6 +47,9 @@
 
 karte_ptr_t tool_t::welt;
 
+// emulate control key by tool
+uint8 tool_t::control_invert = 0;
+
 // for key lookup; is always sorted during the game
 vector_tpl<tool_t *>tool_t::char_to_tool(0);
 
@@ -334,6 +337,7 @@ tool_t *create_simple_tool(int toolnr)
 		case TOOL_CONVOY_NAMEPLATES:    tool = new tool_convoy_nameplate_t();     break;
 		case TOOL_CONVOY_LOADINGBAR:    tool = new tool_convoy_loadingbar_t();    break;
 		case TOOL_SHOW_FACTORY_STORAGE: tool = new tool_show_factory_storage_t(); break;
+		case TOOL_TOGGLE_CONTROL:       tool = new tool_toggle_control_t();       break;
 		case TOOL_TOOGLE_PAX:           tool = new tool_toggle_pax_station_t();   break;
 		case TOOL_TOOGLE_PEDESTRIANS:   tool = new tool_toggle_pedestrians_t();   break;
 		case TOOL_TRAFFIC_LEVEL:        tool = new tool_traffic_level_t();        break;

simtool.h
@@ -1148,6 +1148,23 @@ public:
 	bool is_work_network_safe() const OVERRIDE { return true; }
 };
 
+class tool_toggle_control_t : public tool_t {
+public:
+	tool_toggle_control_t() : tool_t(TOOL_TOGGLE_CONTROL | SIMPLE_TOOL) {}
+	char const* get_tooltip(player_t const*) const OVERRIDE { return translator::translate("Lock Control key"); }
+	bool is_selected() const OVERRIDE { return tool_t::control_invert; }
+	bool init(player_t*) OVERRIDE {
+		tool_t::control_invert = WFL_CTRL;
+		return false;
+	}
+	bool exit(player_t* s) OVERRIDE {
+		tool_t::control_invert = 0;
+		return false;
+	}
+	bool is_init_network_safe() const OVERRIDE { return true; }
+	bool is_work_network_safe() const OVERRIDE { return true; }
+};
+
 class tool_rotate90_t : public tool_t {
 public:
 	tool_rotate90_t() : tool_t(TOOL_ROTATE90 | SIMPLE_TOOL) {}