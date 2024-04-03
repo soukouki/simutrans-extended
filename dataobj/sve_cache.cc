5ed87034f78b9e9d64b30ee208d55b836cb754cd
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2024-04-04 07:20:02 +0900

    FIX: merging conflict

--- dataobj/sve_cache.cc
+++ dataobj/sve_cache.cc
@@ -196,7 +196,7 @@ std::string sve_cache_t::get_most_recent_compatible_save()
 	sint64 best_time = 0;
 
 	for (stringhashtable_tpl<sve_info_t *, N_BAGS_LARGE>::iterator it = cached_info.begin(); it != cached_info.end(); ++it) {
-		if (it->value->pak+"/" == env_t::pak_name && it->value->mod_time > best_time) {
+		if (it->value->pak+"/" == env_t::objfilename && it->value->mod_time > best_time) {
 			best_filename = it->key;
 			best_time = it->value->mod_time;
 		}
