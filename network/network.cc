b25b9506ab5488b539cadcd10fef65b06589933c
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2024-05-14 21:41:59 +0900

    FIX: merging error

--- network/network.cc
+++ network/network.cc
@@ -362,7 +362,7 @@ bool network_init_server( int port, const vector_tpl<std::string> &listen_addrs 
 #else
 
 	// For each address in the list of listen addresses try and create a socket to listen on
-	for(std::string const& ip : ips) {
+	for(std::string const& ip : listen_addrs) {
 		int ret;
 		char port_nr[16];
 		sprintf(port_nr, "%u", port);
