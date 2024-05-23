soukouki/simutrans-extended

commit 4f3616eb72ff3ac82417ae4dd27526134f60bf14

CHG: Public player only appears in the ranking of number of stations owned

gui/player_ranking_gui.cc
@@ -210,7 +210,7 @@ player_ranking_gui_t::player_ranking_gui_t(uint8 selected_player_nr) :
 		cont_players.set_margin(NO_SPACING, scr_size(D_SCROLLBAR_WIDTH+D_H_SPACE,D_SCROLLBAR_HEIGHT));
 
 		for (int np = 0; np < MAX_PLAYER_COUNT; np++) {
-			if (np == PUBLIC_PLAYER_NR) continue;
+			//if (np == PUBLIC_PLAYER_NR) continue; // Public player only appears in the ranking of number of stations owned.
 			if (welt->get_player(np) ) {
 				player_button_t* b = new player_button_t(np);
 				b->add_listener(this);
@@ -319,7 +319,7 @@ void player_ranking_gui_t::sort_player()
 	for (uint i = 0; i < buttons.get_count(); i++) {
 		const uint8 player_nr = buttons.at(i)->get_player_nr();
 		// Exclude players who are not in the competition
-		if( player_nr==PUBLIC_PLAYER_NR || is_chart_table_zero( player_nr ) ) {
+		if( (player_nr==PUBLIC_PLAYER_NR && selected_item!=PR_HALTS) || is_chart_table_zero( player_nr ) ) {
 			continue;
 		}
 		count++;
@@ -508,7 +508,7 @@ void player_ranking_gui_t::update_chart()
 	// need to clear the chart once to update the suffix and digit
 	chart.remove_curves();
 	for (int np = 0; np < MAX_PLAYER_COUNT - 1; np++) {
-		if (np == PUBLIC_PLAYER_NR) continue;
+		if (np == PUBLIC_PLAYER_NR && selected_item != PR_HALTS) continue;
 		if ( player_t* player = welt->get_player(np) ) {
 			if (is_chart_table_zero(np)) continue;
 			// create chart

commit 9f51241650491c90f27610322a05722d21b7d6f4

FIX: Rank display is incorrect
introduced in 4f3616eb72ff3ac82417ae4dd27526134f60bf14

gui/player_ranking_gui.cc
@@ -338,20 +338,24 @@ void player_ranking_gui_t::sort_player()
 	for (uint i = 0; i < buttons.get_count(); i++) {
 		const uint8 player_nr = buttons.at(i)->get_player_nr();
 		// Exclude players who are not in the competition
-		if( (player_nr==PUBLIC_PLAYER_NR && selected_item!=PR_HALTS) || is_chart_table_zero( player_nr ) ) {
+		if( is_chart_table_zero(player_nr) ) {
+			continue;
+		}
+		// Public player only appers in stop number ranking
+		if( player_nr==PUBLIC_PLAYER_NR && selected_item!=PR_HALTS ) {
 			continue;
 		}
 		count++;
 
-		switch (i)
+		switch (count)
 		{
-			case 0:
+			case 1:
 				cont_players.new_component<gui_color_label_t>("1", color_idx_to_rgb(COL_WHITE), 56512, 1);
 				break;
-			case 1:
+			case 2:
 				cont_players.new_component<gui_color_label_t>("2", 0, 36053, 1);
 				break;
-			case 2:
+			case 3:
 				cont_players.new_component<gui_color_label_t>("3", 0, 37702, 1);
 				break;
 